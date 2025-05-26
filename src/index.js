8c0aef27c9f55780c05c87a7cf0c18b425404f7d
akpanu <akpanmichael90@gmail.com>
2025-05-26 20:15:45 +0100

    Committing typescript version of eat-n-split project

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 // import App from './App'
-import Application from './Appts'
+import Appts from './Appts'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     {/* <App /> */}
 
-    <Application />
+    <Appts />
   </React.StrictMode>
 )
